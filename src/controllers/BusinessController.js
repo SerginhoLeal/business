12d3cda9014cbf96b67994a6979bca1b4aeaa782
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-04-11 18:38:43 -0300

    api

--- src/controllers/BusinessController.js
+++ src/controllers/BusinessController.js
@@ -9,44 +9,32 @@ module.exports ={
         return res.json(business);
     },
 
-    async index(req, res){
-        const business = await Bus.find();
-        return res.json(business);
-    },
-
     async login(req, res){
-        const {email, password} = req.body;
+        const {nome, password} = req.body;
 
-        const user = await Bus.findOne({email}).select('+password');
+        const user = await Bus.findOne({nome}).select('+password');
         
         if(!user)
-            return res.status(400).send({error:'Email inexistente'});
+            return res.status(400).send({error:'Nome inexistente'});
         
         if(!await bcrypt.compare(password, user.password))
             return res.status(400).send({error:'Senha invalida'});
         
         user.password = undefined;
 
-        res.send({
-            user,
-            // token:generateToken({id:user.id}),
-        });
+        res.send({user});
     },
 
     async store(req, res){
-        const {email} = req.body;
-        // const desc = descript.split(',').map(no => no.trim());
+        const {nome} = req.body;
     try{
-        if(await Bus.findOne({email}))//se encontrar um email o cadastro não será realizado
-            return res.status(400).send({error:'Email já em uso!'});
+        if(await Bus.findOne({nome}))//se encontrar um email o cadastro não será realizado
+            return res.status(400).send({error:'Nome já em uso!'});
 
         const user = await Bus.create(req.body);
 
         user.password = undefined;
-        return res.send({
-            user,
-            // token:generateToken({id:user.id}),
-        });
+        return res.send({user});
         }catch(err){
             return res.statusCode(400).send({error:'fail'});
         }
