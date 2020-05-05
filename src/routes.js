ae1a39f976e9ff7a2b05fbf1d6f6ca1e4ae37c3a
SerginhoLeal <48488987+SerginhoLeal@users.noreply.github.com>
2020-05-05 11:49:08 -0300

    api

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ routes.post('/NRBQlog6f2Pwnqe3adQJ', BusinessController.store);
 
 routes.get('/8dr7YKjlJ3aXKcnwGJrm', SearchController.index);
 routes.post('/C7Ypo2iFU0OTT7RrH1TR', SearchController.store);
-routes.delete('/tuo5NSqTcZ7fXUKBMtGh/:id', SearchController.destroy);
+routes.delete('/tuo5NSqTcZ7fXUKBMtGh/:id/5NSqTcZ7', SearchController.destroy);
 
 routes.get('/C7YpoRrH1TR2iFU0OTT7', PesquisarController.index2);
 routes.get('/kW24SJmbA6surYp5qWPJ', PesquisarController.index);
